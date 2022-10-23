56b5ae17c137c08206337c4fc1fd953f9b167f56
not-rahim <rahimejaz.234@gmail.com>
2022-10-23 13:57:04 +0500

    'inic'

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ module.exports = {
   port: 8080,
   db: {
     
-    prod: process.env.DATABASE_URL || 'mongodb+srv://jameskobulyar:<james123>@cluster0.qyxuyxt.mongodb.net/?retryWrites=true&w=majority',
-    test: 'mongodb+srv://jameskobulyar:<james123>@cluster0.qyxuyxt.mongodb.net/?retryWrites=true&w=majority',
+    prod: process.env.DATABASE_URL || 'mongodb+srv://jameskobulyar:james123@cluster0.qyxuyxt.mongodb.net/?retryWrites=true&w=majority',
+    test: 'mongodb+srv://jameskobulyar:james123@cluster0.qyxuyxt.mongodb.net/?retryWrites=true&w=majority',
     options: {
       useNewUrlParser: true,
      
