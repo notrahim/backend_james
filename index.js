c4d6088fe3638aa6c4ff0f5a209dbf5fe11cbe3d
not-rahim <rahimejaz.234@gmail.com>
2022-10-23 20:38:29 +0500

    'l'

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const connect = async () => {
 }
 
 if (require.main === module) {
-  app.listen(process.env.PORT || 5000);
+  app.listen(process.env.PORT || 3000 );
  connect()
   mongoose.connection.on('error', console.log);
 }
